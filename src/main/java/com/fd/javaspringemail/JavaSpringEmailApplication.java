812f09cadccacfef064a0b36d3f272d7e3c0ec52
Fransii <fransiid@gmail.com>
2018-03-13 21:37:38 +0100

    Add main class

---
+++ src/main/java/com/fd/javaspringemail/JavaSpringEmailApplication.java
@@ -0,0 +1,12 @@
+package com.fd.javaspringemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringEmailApplication.class, args);
+	}
+}
